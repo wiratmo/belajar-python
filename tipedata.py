bf459c38d3743e6361a80efa99035511999ef35d
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:20:20 +0700

    materi 2. Tipe data

---
+++ tipedata.py
@@ -0,0 +1,11 @@
+nama = "alexander"
+usia = 26
+berat_badan = 59.7
+is_diet = True
+nilai = [80, 79, 60]
+
+print(type(nama)) # str/ string
+print(type(usia)) # int/ integer
+print(type(berat_badan)) # double/ float / bilangan desimal
+print(type(is_diet)) # boolean
+print(type(nilai)) # list
