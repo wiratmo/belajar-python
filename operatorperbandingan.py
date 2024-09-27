df20183fc9f32a1683bb314e305330f9c9af9e43
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:21:02 +0700

    materi 3. Operator perbandingan

---
+++ operatorperbandingan.py
@@ -0,0 +1,15 @@
+angka1 = 9
+angka2 = 6
+angka3 = 10
+angka4 = 2
+
+# operator perbandingan nilainya hanya ada 2 => true or false (benar atau salah)
+
+# sama dengan
+print(angka1 == angka1) # True/ benar
+print(angka1 != angka1) # False/ salah
+# print(angka3 <> angka2) # True / benar 
+print(angka2 > angka3) # False
+print(angka3 < angka3) # False
+print(angka4 >= angka4) # True
+print(angka3 <= angka4) # False
