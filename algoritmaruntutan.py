22aaf647ce7f4a9759b54e56e38a858d6725cae6
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 22:13:39 +0700

    materi 5. Struktur algoritma runtutan

---
+++ algoritmaruntutan.py
@@ -0,0 +1,10 @@
+# luas persegi panjang
+# input
+panjang = int(input("masukkan panjang persegi panjang : "))
+lebar = int(input("masukkan lebar persegi panjang : "))
+
+# proses
+luas = panjang * lebar
+
+# output dengan bantuan f string
+print(f"luas persegi panjang dengan ukuran panjang = {panjang} dan lebar = {lebar} adalah {luas} ")
