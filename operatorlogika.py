0b0df05c2a62cd6f31179bc85e71c9c91a1f061b
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:21:44 +0700

    materi 3. Operator logika

---
+++ operatorlogika.py
@@ -0,0 +1,8 @@
+# Logika and akan bernilai benar saat semuanya benar
+print(True and False) # False 
+# logika or akan bernilai salah saat semuanya salah
+print(False or True) # True
+# logikanya berkebalikan
+print(not False) # True
+
+print( (6 != 5) or (5 > 8)) # true or false => true
