e97003a369bbaa81a7d501c4244382850227ec13
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:20:46 +0700

    materi 3. Operator aritmatika

---
+++ operator.py
@@ -0,0 +1,20 @@
+angka1 = 6
+angka2 = 3
+angka3 = 10
+angka4 = 2
+
+# operasi penjumlahan
+print(angka1 + angka2) # 9
+# operasi pengurangan
+print(angka1 - angka4) # 4
+# operasi perkalian
+print(angka3 * angka4) # 20
+#operasi pembagian
+print(angka1 / angka4) # 3
+#operasi sisa hasil bagi
+print(angka3 % angka2) # 1
+#operasi pangkat
+print(angka4 ** angka2) #8
+#operasi pembagian bulat
+print(angka3 // angka2) #3
+print(angka3 / angka2) #3.33333...
