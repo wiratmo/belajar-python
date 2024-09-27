793a8712130ed88caf13c100be8932a236368274
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 22:01:34 +0700

    materi 4. Perintah input dan perintah print dengan beberapa penggunaan

---
+++ perintahinput.py
@@ -0,0 +1,9 @@
+nama = "fatimah"
+alamat = input("masukkan alamat anda ")
+berat = int(input("masukkan berat badan anda "))
+tinggi = int(input("masukkan tinggi badan anda "))
+print(nama)
+print(alamat)
+print(type(berat))
+print(type(tinggi))
+print(berat * tinggi)
---
+++ perintahoutput.py
@@ -0,0 +1,14 @@
+nama = input("nama anda : ")
+usia = input("usia anda dalam satuan tahun : ")
+
+# mencetak hanya kata saja
+print("selamat datang")
+
+#mencetak dengan variabel
+print("nama saya", nama, "usia saya", usia)
+
+# mencetak dengan bantuan format
+print("nama saya {} usia saya {} tahun".format(nama, usia))
+
+# mencetak dengan f string
+print(f"nama saya {nama} usia saya {usia} tahun")
