f908da9622a38d2d0a1d539aa276fc10f34de330
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:18:43 +0700

    materi 1. Belajar perintah cetak dan komentar

---
+++ perintah_cetak_komentar.py
@@ -0,0 +1,9 @@
+# komentar tidak akan dicetak oleh mesin
+# fungsi dari komentar sebagai label saja hanya untuk menjadi penanda
+
+# cetak nama 
+print("Nama saya adalah Agung Wiratmo")
+# cetak tanggal lahir
+print("Tanggal Lahir saya adalah 20 sept 2004")
+# cetak hobi
+print("Hobi saya adalah bermain")
