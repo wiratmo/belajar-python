baa6b0d7fda355f276dee3edcfe891e8d8c319be
wiratmo <4a.wiratmo@gmail.com>
2024-09-27 21:21:31 +0700

    materi 3. Operator penugasan

---
+++ operatorpenugasan.py
@@ -0,0 +1,26 @@
+angka = 6
+print(angka) # 6
+# variabel angka yang baru sama dengan nilai dari variabel angka lama 
+#                                                 ditambah
+#                                                 dua
+# angka = angka + 2 
+# print(angka) # 8
+# variabel angka yang baru sama dengan nilai dari variabel angka lama 
+#                                                 ditambah
+#                                                 dua
+# tambah sama dengan
+angka += 2
+print(angka) # 8
+# kurang sama dengan
+angka -= 6
+print(angka) # 8-6 = 2 
+# perkalian sama dengan
+angka *= 3
+print(angka) # 6
+
+# pembagian sama dengan
+angka /= 2
+print(angka) # 3
+
+
+# operator penugasan akan digunakan saat algaoritma perulangan -> while
