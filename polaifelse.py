fce3b7ddb451e9bee746e7b9056ee60394199392
wiratmo <4a.wiratmo@gmail.com>
2024-09-29 07:53:33 +0700

    materi 5. pola algoritma if else

---
+++ polaifelse.py
@@ -0,0 +1,14 @@
+# pola pengkodean if else
+
+nilai = 5
+
+# percabangan dengan kondisi nilai lebih besar deari 7
+if (nilai > 7):
+    print("lulus")
+# tetapi akan dijalankan saat pernyataan diatas bernilai salah
+else:
+    # print "tidas lulus akan dijalankan jika kondisi di atas bernilai salah"
+    print("tidak lulus")
+
+# note: bagaimana jika tidak cukup dengan dua kondisi, misal mengkategorikan nilai menjadi
+#       kategori a, b, c, dst?
