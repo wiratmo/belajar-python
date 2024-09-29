357fbae093d89931a3c6d335b972e7e4674c4de4
wiratmo <4a.wiratmo@gmail.com>
2024-09-29 07:52:36 +0700

    materi 5. pola algoritma if

---
+++ polaif.py
@@ -0,0 +1,12 @@
+# pola pengkodean if
+
+# variabel nilai
+nilai = 9
+
+# percabangan dengan kondisi nilai lebih besar deari 7
+if (nilai > 7):
+    # print "lulus" akan dijalankan jika kondisi diatas bernilai benar
+    print("lulus")
+
+# note: bagaimana jika nilai kurang dari 7?
+#       maka tidak ada yang tercetak
