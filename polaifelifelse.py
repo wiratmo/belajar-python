2dd53a282570adf212a45b231fcce7b601c5e2b9
wiratmo <4a.wiratmo@gmail.com>
2024-09-29 07:53:51 +0700

    materi 5. pola algoritma if elif else

---
+++ polaifelifelse.py
@@ -0,0 +1,18 @@
+# pola pengkodean if elif else
+nilai = 8
+# percabangan dengan kondisi nilai lebih besar deari 9
+if (nilai > 9):
+    print("A")
+# tetapi jika nilai lebih besar dari 7 akan dijalankan ASAL PERNYATAAN DI ATAS BERNILAI SALAH
+elif (nilai > 7):
+    print("B")
+# tetapi jika nilai lebih besar dari 5 akan dijalankan ASAL PERNYATAAN DI ATAS BERNILAI SALAH
+elif (nilai > 5):
+    print("C")
+# tetapi jika nilai lebih besar dari 3 akan dijalankan ASAL PERNYATAAN DI ATAS BERNILAI SALAH
+elif (nilai > 3):
+    print("D")
+# jika semua kondisi salah maka E akan dicetak
+else:
+    print("E")
+# note: apa bedanya IF dan ELIF
