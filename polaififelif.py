d889aa3b28a0e37d5af4da73d0a4d96bc260822a
wiratmo <4a.wiratmo@gmail.com>
2024-09-29 07:54:05 +0700

    materi 5. pola algoritma if if else

---
+++ polaififelif.py
@@ -0,0 +1,17 @@
+# pola pengkodean if if else
+nilai = 8
+# percabangan dengan kondisi nilai lebih besar deari 9
+if (nilai > 9):
+    print("A")
+# jika nilai lebih besar dari 7 AKAN SELALU dijalankan
+if (nilai > 7):
+    print("B")
+# jika nilai lebih besar dari 5 AKAN SELALU dijalankan
+if (nilai > 5):
+    print("C")
+# jika nilai lebih besar dari 3 AKAN SELALU dijalankan
+if (nilai > 3):
+    print("D")
+# jika kondisi tepat diatasnya salah maka E akan dicetak
+else:
+    print("E")
