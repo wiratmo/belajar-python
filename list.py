94b0f05f0ad0d7f93b92c37bac488d5a48b4948b
wiratmo <4a.wiratmo@gmail.com>
2024-09-30 07:57:50 +0700

    tipe data list

---
+++ list.py
@@ -0,0 +1,11 @@
+# kota = ['karanganyar', 'sragen', 'surakarta']
+# print(kota[2])
+
+angka = []
+bil1 =int(input("masukan angka pertama : "))
+bil2 = int(input("masukan angka kedua : "))
+# function append
+angka.append(bil1)
+angka.append(bil2)
+print(angka)
+print(angka[0]+ angka[1])
