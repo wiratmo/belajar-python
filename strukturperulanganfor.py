1207309c7038219c546995d0db046ff1229e6bcb
wiratmo <4a.wiratmo@gmail.com>
2024-09-30 07:58:29 +0700

    materi 7. struktur algoritma perulangan for

---
+++ strukturperulanganfor.py
@@ -0,0 +1,9 @@
+# angka = [9, 5, 10, 67, 200]
+# # untuk setiap x didalam list angka
+# for x in angka:
+#     # dicetak nilai setiap x
+#     print(x)
+
+# range 10 dimaulai dari 0 s.d. 9
+for x in range(10):
+    print(x+1)
